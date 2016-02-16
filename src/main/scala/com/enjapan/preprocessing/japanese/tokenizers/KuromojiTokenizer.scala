f098f045d5df8b6f95d666fd918b29f434e78ec1
Mateusz Dymczyk <dymczyk@gmail.com>
2016-02-16 23:27:37 +0900

    Add API to get sequence of tags

--- src/main/scala/com/enjapan/preprocessing/japanese/tokenizers/KuromojiTokenizer.scala
+++ src/main/scala/com/enjapan/preprocessing/japanese/tokenizers/KuromojiTokenizer.scala
@@ -6,31 +6,37 @@ import scala.collection.JavaConverters._
 /**
   * Created by Ugo Bataillard on 2/10/16.
   */
-class KuromojiTokenizer(tokenizer:Tokenizer = new Tokenizer(), stopWords: Set[String] = Set.empty[String], whiteListPOS: Set[List[String]] = Set.empty[List[String]], stopPOS: Set[List[String]] = Set.empty[List[String]]) {
+class KuromojiTokenizer(tokenizer: Tokenizer = new Tokenizer(), stopWords: Set[String] = Set
+  .empty[String], whiteListPOS: Set[List[String]] = Set.empty[List[String]], stopPOS: Set[List[String]] = Set
+  .empty[List[String]]) {
 
   import KuromojiTokenizer._
 
-  def tokenize(string: String): Seq[String] = {
+  def tokenizeWithMetadata(string: String): Seq[Token] = {
     val tokens: Seq[Token] = tokenizer.tokenize(string).asScala.toSeq
     tokens
       .filterNot(matchWords(stopWords))
       .filterNot(unrecognizedPOS)
       .filter(t => whiteListPOS.isEmpty || matchPOSMasks(whiteListPOS)(t))
       .filterNot(matchPOSMasks(stopPOS))
-      .flatMap { t =>
-          Option(t.getBaseForm).filterNot(_.contains("*")).filterNot(_.trim.isEmpty)
-          .orElse(Option(t.getSurface).filterNot(_.trim.isEmpty))
-      }
   }
+
+  def tokenize(string: String): Seq[String] = {
+    val tokens: Seq[Token] = tokenizer.tokenize(string).asScala.toSeq
+    tokenizeWithMetadata(string).flatMap(extractWord)
+  }
+
+  def extractWord(t: Token) = Option(t.getBaseForm).filterNot(_.contains("*")).filterNot(_.trim.isEmpty)
+    .orElse(Option(t.getSurface).filterNot(_.trim.isEmpty))
 }
 
 object KuromojiTokenizer {
 
-  def matchWords(words:Set[String])(token:Token):Boolean = {
+  def matchWords(words: Set[String])(token: Token): Boolean = {
     words.nonEmpty && words.intersect(Set(token.getSurface, token.getBaseForm)).nonEmpty
   }
 
-  def matchPOSMasks(posMasks:Set[List[String]])(token:Token):Boolean = {
+  def matchPOSMasks(posMasks: Set[List[String]])(token: Token): Boolean = {
     posMasks.nonEmpty && {
       val pos = List(token.getPartOfSpeechLevel1, token.getPartOfSpeechLevel2, token.getPartOfSpeechLevel3, token
         .getPartOfSpeechLevel4)
@@ -39,7 +45,7 @@ object KuromojiTokenizer {
     }
   }
 
-  def unrecognizedPOS(token:Token): Boolean = {
+  def unrecognizedPOS(token: Token): Boolean = {
     token.getPronunciation == "*" &&
       token.getPartOfSpeechLevel1 == "名詞" &&
       token.getPartOfSpeechLevel2 == "サ変接続"
