c62479dbe8622f51eaa40f31ae4ea60bf307da5b
Mateusz Dymczyk <dymczyk@gmail.com>
2016-02-17 10:40:48 +0900

    Predicate to PA tokens tests

--- src/test/scala/com/enjapan/preprocessing/japanese/tokenizers/KNPTokenizerTest.scala
+++ src/test/scala/com/enjapan/preprocessing/japanese/tokenizers/KNPTokenizerTest.scala
@@ -1,6 +1,6 @@
 package com.enjapan.preprocessing.japanese.tokenizers
 
-import com.enjapan.knp.models.{Pas, Tag}
+import com.enjapan.knp.models.{PredicateArgumentAnalysis, Pas, Tag}
 import com.enjapan.preprocessing.exceptions.PredicateArgumentsSizeException
 import org.scalatest.{FunSuite, Matchers}
 
@@ -19,7 +19,8 @@ class KNPTokenizerTest extends FunSuite with Matchers {
 
   test("Should modal predicate containing all modals") {
     val cfid = "cfid"
-    val tag = new Tag(0, "", "", null, Seq(), KNPTokenizer.MODALS.map(value => (value,"")).toMap, Seq(), Some(new Pas(cfid, Map())))
+    val tag = new Tag(0, "", "", null, Seq(), KNPTokenizer.MODALS.map(value => (value, ""))
+      .toMap, Seq(), Some(new Pas(cfid, Map())))
     val modalPredicate = knpTokenizer.predicateToToken(tag)
     modalPredicate should be(("-" + KNPTokenizer.MODALS.mkString("-")) + cfid)
   }
@@ -31,4 +32,35 @@ class KNPTokenizerTest extends FunSuite with Matchers {
     modalPredicate should be(cfid)
   }
 
+  test("Should fail predicate to PA on no predicate arguments") {
+    intercept[PredicateArgumentsSizeException] {
+      knpTokenizer.predicateToPATokens(Set())(new Tag(0, "", "", null, Seq(), Map(), Seq(), None))
+    }
+  }
+
+  test("Should return PA tokens for all PA arguments") {
+    val cfid = "cfid"
+    val tag = new Tag(
+      0, "", "", null, Seq(),
+      Map("否定表現" -> ""), Seq(),
+      Some(new Pas(cfid, Map("test" -> new PredicateArgumentAnalysis("relname", "reltype", "argword", 0, "0")))))
+    val tokens = knpTokenizer.predicateToPATokens(Set())(tag)
+    tokens should have size 1
+    tokens.head should be("-否定表現cfid_argword_relname")
+  }
+
+  test("Should filter arguments by relation surface") {
+    val cfid = "cfid"
+    val tag = new Tag(
+      0, "", "", null, Seq(),
+      Map("否定表現" -> ""), Seq(),
+      Some(new Pas(cfid, Map(
+        "test" -> new PredicateArgumentAnalysis("relname", "reltype", "argword", 0, "0"),
+        "test2" -> new PredicateArgumentAnalysis("nottobetakenintoaccount", "reltype", "argword", 0, "0")
+      )))
+    )
+    val tokens = knpTokenizer.predicateToPATokens(Set("relname"))(tag)
+    tokens should have size 1
+    tokens.head should be("-否定表現cfid_argword_relname")
+  }
 }
